59cfdcd2cd26d7202395cefeb88bc95daf0bfc93
vvb2060 <vvb2060@gmail.com>
2023-02-19 21:59:29 +0800

    Use JDK 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,6 @@ allprojects {
     group = "org.lsposed.lsparanoid"
     version = "0.4.5"
 
-    val javaVersion by extra(JavaVersion.VERSION_11)
+    val javaVersion by extra(JavaVersion.VERSION_17)
     val androidToolsVersion by extra("7.4.1")
 }
