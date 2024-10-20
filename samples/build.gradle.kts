04b823ea81d61f6213cef627bc2cc510e0053931
Howard Wu <HowardWu20@outlook.com>
2024-10-20 23:04:29 +0800

    fix: Upgrade Gradle to fix sample build

--- samples/build.gradle.kts
+++ samples/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application") version "7.4.2" apply false
-    id("com.android.library") version "7.4.2" apply false
+    id("com.android.application") version "8.7.1" apply false
+    id("com.android.library") version "8.7.1" apply false
     id("org.lsposed.lsparanoid") version "0.6.0" apply false
 }
